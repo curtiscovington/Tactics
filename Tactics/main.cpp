5daad4617f38555d42d553944eea91348f85d2d6
Curtis Covington <curtis.covington@gmail.com>
2016-01-01 16:49:14 -0500

    Initial Commit

---
+++ Tactics/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tactics
+//
+//  Created by Curtis Covington on 1/1/16.
+//  Copyright © 2016 Curtis Covington. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
